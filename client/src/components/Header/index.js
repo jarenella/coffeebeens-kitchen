498007fbc64c5dbac8827532afb885d8626e8fac
jarenella <jamesa122333@gmail.com>
2023-05-22 11:11:30 -0700

    header and navbar style

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,7 +2,15 @@ import './Header.css';
 
 function Header() {
   return (
-    <h1>CoffeeBeen's Kitchen</h1>
+    <header>
+      <h1 className="">CoffeeBeen's Kitchen</h1>
+      <nav className="header-nav">
+        <a className="navlink">Home</a>
+        <a className="navlink">Recipes</a>
+        <a className="navlink">About</a>
+        <a className="navlink">Contact</a>
+      </nav>
+    </header>
   );
 }
 
